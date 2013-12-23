a3a8a6141301d68109c063aaeac2611c5cbad51f
Phuoc Do <dnprock@gmail.com>
2013-12-22 21:50:10 -0800

    Add files

---
+++ package.js
@@ -0,0 +1,12 @@
+Package.describe({
+  summary: "three.js package for meteor"
+});
+
+Package.on_use(function (api) {
+  api.use('jquery', 'client');
+  api.add_files([
+      'lib/three.js/three.js'
+    ]
+    ,'client'
+  );
+});
